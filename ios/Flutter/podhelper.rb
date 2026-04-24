de38a80e7eb0744b316db8ebdb2435c7162c7d56
limebob1985 <robyalaimo85@gmail.com>
2026-04-24 23:50:06 +0200

    Inseriti manualmente i file mancanti per Codemagic

---
+++ ios/Flutter/podhelper.rb
@@ -0,0 +1,11 @@
+def flutter_install_all_ios_pods(ios_application_path = nil)
+  flutter_application_path ||= File.join(ios_application_path, '..')
+  pubspec = YAML.load_file(File.join(flutter_application_path, 'pubspec.yaml'))
+  return if pubspec.nil?
+end
+
+def flutter_additional_ios_build_settings(target)
+  target.build_configurations.each do |config|
+    config.build_settings['ENABLE_BITCODE'] = 'NO'
+  end
+end
